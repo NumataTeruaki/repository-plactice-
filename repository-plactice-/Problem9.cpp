afb49e111d4a8cc5863643adac636b42d7d20dee
NumataTeruaki <numatateruaki@hotmail.co.jp>
2014-06-19 15:55:51 +0900

    １５時５１分

---
+++ repository-plactice-/Problem9.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+using namespace std;
+
+
+int main(void)
+{
+	for(int i = 1; i <= 1000; i++){
+		for(int j = 1; j <= 1000; j++){
+			int c = 1000-i-j;
+
+			if(c > 0 && i*i+j*j == c*c){
+				cout << i*j*c << endl;
+				return 0;
+			}
+		}
+	}
+}
+
